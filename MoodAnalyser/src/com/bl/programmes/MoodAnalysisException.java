65a06bccf34951e0979fd38a70fc16c623fd7eaa
anzar shaikh <anzarshaikh373@gmail.com>
2021-07-16 21:30:54 +0530

    Mood analyser with test case is done. 16-07-2021

---
+++ MoodAnalyser/src/com/bl/programmes/MoodAnalysisException.java
@@ -0,0 +1,10 @@
+package com.bl.programmes;
+
+public class MoodAnalysisException extends Exception {
+    enum ExceptionType {ENTERED_NULL,ENTERED_EMPTY}
+    ExceptionType type;
+    public MoodAnalysisException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+}
